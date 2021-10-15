95ef41731fca4bb7badf7d81a1053d040310b286
cyn4hia <up2cindy@gmail.com>
2021-10-15 22:37:12 +0000

    helloworld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main (){
+        System.out.println("Hello Java!");
+        int i = 2;
+        i = i*4;
+        System.out.println(i);
+    }
+}
